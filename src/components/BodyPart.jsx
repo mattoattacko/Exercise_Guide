b69ddcc90f1fcdd378a6c3035940d803eabfb79f
unknown <matt.mcquain@gmail.com>
2022-06-14 21:25:10 -0700

    API hooked up. HorizontalScrollbar working but still need to change styling on arrow buttons with large displays. Starting on Exercises component.

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material'
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png'
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+      type='button'
+      alignItems='center'
+      justifyContent='center'
+      className='bodyPart-card'
+      sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',// styles for the currently selected icon, else we style them slightly differently
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px'
+      }}
+        onClick={() => {
+          setBodyPart(item);
+          window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+      }}
+    >
+      <img src={Icon} alt='gym' style={{ width: '40px', height: '40px' }} />
+      <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>
+        {item}
+      </Typography>
+    </Stack>
   )
 }
 
 export default BodyPart
+
